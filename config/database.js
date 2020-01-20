647735b0341457703a933c82421179766f55a897
PramodSaw <51080241+PramodSaw@users.noreply.github.com>
2020-01-20 18:04:45 +0530

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -2,6 +2,8 @@ const crypto = require('crypto').randomBytes(256).toString('hex');
 module.exports = {
     //uri: 'mongodb://localhost:27017/mean-stack',
 	uri: 'mongodb+srv://Test:Test@123@cluster0-nb5lj.mongodb.net/angularjsblogs?retryWrites=true&w=majority',
+	//uri: 'mongodb+srv://Test:Test@123@cluster0-nb5lj.mongodb.net/angularjsblogs?retryWrites=true&w=majority',
+    uri: 'mongodb://root:password@ds027215.mlab.com:27215/gugui3z24',
     secret: crypto,
     db: 'mean-stack'
 }
