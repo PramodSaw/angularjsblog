85dd7e1afb9dc539c4cc08c522bfe2f1a35172f4
PramodSaw <51080241+PramodSaw@users.noreply.github.com>
2021-10-16 18:17:36 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,8 +2,7 @@ const crypto = require('crypto').randomBytes(256).toString('hex');
 module.exports = {
     //uri: 'mongodb://localhost:27017/mean-stack',
     //uri: 'mongodb+srv://Test:Test@123@cluster0-nb5lj.mongodb.net/angularjsblogs?retryWrites=true&w=majority',
-   // uri: 'mongodb+srv://Test:Test:Test@123@cluster0-nb5lj.mongodb.net/angularjsblogs',
-    uri: 'mongodb+srv://Test:D3TwtIE2TTa11Yob@cluster0.nb5lj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+      uri: 'mongodb+srv://Test:Test:Test@123@cluster0-nb5lj.mongodb.net/angularjsblogs',
     //uri: 'mongodb://root:password@ds027215.mlab.com:27215/gugui3z24',
     secret: crypto,
     //db: 'mean-stack'
